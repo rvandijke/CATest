470028e722eaabe2b2042cec054b430b33895c52
Robin van Dijke <robin@trvl.com>
2012-10-09 08:37:44 +0200

    first commit

---
+++ CATest/Layer.h
@@ -0,0 +1,13 @@
+//
+//  Layer.h
+//  CATest
+//
+//  Created by Robin van Dijke on 09-10-12.
+//  Copyright (c) 2012 Robin van Dijke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Layer : CALayer
+
+@end
---
+++ CATest/Layer.m
@@ -0,0 +1,13 @@
+//
+//  Layer.m
+//  CATest
+//
+//  Created by Robin van Dijke on 09-10-12.
+//  Copyright (c) 2012 Robin van Dijke. All rights reserved.
+//
+
+#import "Layer.h"
+
+@implementation Layer
+
+@end
